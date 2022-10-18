46a8ec75fa7b8e6ea81106169ea9b2b6f8356e1c
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-17 23:00:14 -0700

    resource list optimisations

--- src/routes/resources/page/[page]/+page.ts
+++ src/routes/resources/page/[page]/+page.ts
@@ -1,4 +1,4 @@
 export function load({params, url}) {
     console.log(url.searchParams.get('sort'))
-    return fetch("https://api.spiget.org/v2/resources?size=50&sort=-" + (url.searchParams.get('sort') || "updateDate") + "&page=" + params.page).then(r => r.json());
+    return fetch("https://api.spiget.org/v2/resources?size=30&fields=name,tag,likes,file,testedVersions,rating,icon,author,category,downloads&sort=-" + (url.searchParams.get('sort') || "updateDate") + "&page=" + params.page).then(r => r.json());
 }
