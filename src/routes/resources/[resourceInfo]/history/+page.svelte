9ed52200b709310432b830a0bd44f9e40a0a23ff
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-20 14:45:29 -0700

    Full version history page

--- src/routes/resources/[resourceInfo]/history/+page.svelte
+++ src/routes/resources/[resourceInfo]/history/+page.svelte
@@ -1,6 +1,32 @@
 <script>
+    import DateStamp from "$lib/DateStamp.svelte";
+    import {commas} from "$lib/utils";
+    import Stars from "$lib/Stars.svelte";
+
     export let data;
 </script>
-{#each data.versions as version}
-    {version.name}<br>
-{/each}
+<style>
+    th.download {
+        color: rgba(0, 0, 0, 0);
+    }
+</style>
+<table class="table">
+    <tbody>
+    <tr>
+        <th class="version">Version</th>
+        <th class="releaseDate">Release Date</th>
+        <th class="downloads">Downloads</th>
+        <th class="rating">Average Rating</th>
+        <th class="download">Download Link</th>
+    </tr>
+        {#each data.versions as version (version.id)}
+            <tr>
+                <td class="version">{version.name}</td>
+                <td class="releaseDate"><DateStamp epochSeconds={version.releaseDate}/></td>
+                <td class="downloads">{commas(version.downloads)}</td>
+                <td class="rating"><Stars rating={version.rating.average}/> {version.rating.count} ratings</td>
+                <td class="download"><a href="https://api.spiget.org/v2/resources/{version.resource}/versions/{version.id}/download">Download</a></td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
--- src/routes/resources/[resourceInfo]/history/+page.ts
+++ src/routes/resources/[resourceInfo]/history/+page.ts
@@ -3,7 +3,7 @@
 // @ts-ignore
 export async function load({ parent }) {
     const resource = await parent();
-    const versions = await fetch("https://api.spiget.org/v2/resources/" + resource.id + "/versions?size=100000&fields=id&sort=-releaseDate").then(r => r.json());
+    const versions = await fetch("https://api.spiget.org/v2/resources/" + resource.id + "/versions?size=100000&sort=-releaseDate").then(r => r.json());
 
     return {
         ...resource,
