64e3e64f6757dc5e3c3871d6007d46cafcbff526
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-21 11:01:45 -0700

    Revert "dont force page reload for redirect"
    This reverts commit a16eca06ec0cefb678af5e9c32e490b173105766.

--- src/routes/resources/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import {onMount} from "svelte";
-    import {goto} from "$app/navigation";
-
-    onMount(() => {
-        goto("/resources/page/1" + location.search);
-    });
-</script>
