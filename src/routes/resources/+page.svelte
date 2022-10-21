a16eca06ec0cefb678af5e9c32e490b173105766
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-21 10:57:16 -0700

    dont force page reload for redirect

---
+++ src/routes/resources/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import {onMount} from "svelte";
+    import {goto} from "$app/navigation";
+
+    onMount(() => {
+        goto("/resources/page/1" + location.search);
+    });
+</script>
