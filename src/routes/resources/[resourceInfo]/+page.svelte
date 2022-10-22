e7dd17b298a154a7d86a7c8b3e4e49a99c3d0ad8
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-22 15:27:04 -0700

    resource description in meta tag

--- src/routes/resources/[resourceInfo]/+page.svelte
+++ src/routes/resources/[resourceInfo]/+page.svelte
@@ -3,6 +3,9 @@
 
     export let data;
 </script>
+<svelte:head>
+    <meta name="description" content={data.tag}>
+</svelte:head>
 <div class="bb-content">
     {@html decodeBase64Content(data.description)}
 </div>
