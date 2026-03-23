62264d558270e2b0252bd6d2758a67c9c605b95f
ajgeiss0702 <aj@ajg0702.us>
2026-03-23 15:49:42 -0700

    start to get ready for cloudflare migration

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,8 +9,13 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			edge: true,
-			split: true
+			include: [
+				"/proxy/image",
+				"/resources/*",
+				"/test",
+				"/users/*",
+				"/"
+			]
 		})
 	}
 };
