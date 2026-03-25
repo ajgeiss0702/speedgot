573262be69ee7c46363dc871fa4a21dca33fde4c
ajgeiss0702 <aj@ajg0702.us>
2026-03-25 16:48:09 -0700

    log requests

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,26 @@
+import {AnalyticsEngineDataset} from "@cloudflare/workers-types"
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+declare global {
+    namespace App {
+        // interface Locals {}
+        // interface PageData {}
+        // interface Error {}
+        interface Platform {
+            env?: {
+                PAGES_REQUESTS?: AnalyticsEngineDataset;
+            },
+            context?: {
+                /**
+                 * Waits for the promise to complete without blocking.
+                 * @param promise The promise that is ensured completion
+                 */
+                waitUntil: (promise: Promise) => void
+            },
+            cf?: IncomingRequestCfProperties,
+            caches: CacheStorage
+        }
+    }
 }
---
+++ src/hooks.server.ts
@@ -0,0 +1,23 @@
+import type { Handle } from "@sveltejs/kit";
+import {dev} from "$app/environment";
+
+export const handle: Handle = async ({ event, resolve }) => {
+    const ua = event.request.headers.get("user-agent");
+
+    const response = await resolve(event);
+
+    const pagesRequests = event.platform?.env?.PAGES_REQUESTS;
+    if(pagesRequests) pagesRequests.writeDataPoint({
+        blobs: [
+            "speedgot",
+            event.url.pathname,
+            ua,
+            response.status + ""
+        ],
+        doubles: [],
+        indexes: ["speedgot"]
+    });
+    if(!pagesRequests && !dev) console.warn("Missing pages requests object!")
+
+    return response;
+}
