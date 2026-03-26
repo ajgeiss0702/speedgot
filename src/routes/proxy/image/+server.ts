42057796318e1d510d4e527a2500e9dc327d8f96
ajgeiss0702 <aj@ajg0702.us>
2026-03-26 14:45:30 -0700

    refuse to proxy non-image requests

--- src/routes/proxy/image/+server.ts
+++ src/routes/proxy/image/+server.ts
@@ -1,13 +1,23 @@
 import {error} from "@sveltejs/kit";
 
-
 export async function GET({ url }) {
-    if(!url.searchParams.get("url")) {
+
+    const fetchingUrl = url.searchParams.get("url");
+    if(!fetchingUrl) {
         throw error(400, "no url");
     }
-    const fetchingUrl: string = url.searchParams.get("url");
 
-    const imageResponse = await fetch(fetchingUrl);
+    const imageResponse = await fetch(fetchingUrl, {
+        headers: {
+            "User-Agent": "Mozilla/5.0 (compatible; Speedgot-image-proxy/1.0.0; +https://speedgot.ajg0702.us) CFNetwork"
+        }
+    });
+
+    if(!imageResponse.headers.get("content-type")?.startsWith("image/")) {
+        console.warn("Non-image requested:", fetchingUrl, imageResponse.headers.get("content-type")+"", imageResponse.status, imageResponse.statusText);
+        imageResponse.body?.cancel();
+        throw error(400, "Not an image!");
+    }
 
     const blob = await imageResponse.blob();
 
