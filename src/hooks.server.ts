2ac0a3495ccf990d721b876ed3c1d4c217826a09
ajgeiss0702 <aj@ajg0702.us>
2026-07-11 19:41:09 -0700

    save d1 reads

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,6 +7,10 @@ let lastLocalYandexRequest = 0;
 export const handle: Handle = async ({ event, resolve }) => {
     const ua = event.request.headers.get("user-agent");
     if(ua?.includes("yandex.com/bots")) {
+        // check local before d1 to save d1 reads for when its being super spammy
+        if(Date.now() - lastLocalYandexRequest < 55e3) {
+            throw error(429, "You are sending too many requests! Please respect the crawl-delay")
+        }
         const db = event.platform?.env?.DB;
         const lastD1YandexRequest = (db && await retryD1(() =>
             db.prepare("select lastRequest from bots where bot = ?")
@@ -15,6 +19,7 @@ export const handle: Handle = async ({ event, resolve }) => {
         )) ?? 0;
         const lastYandexRequest = Math.max(lastLocalYandexRequest, lastD1YandexRequest);
         if(Date.now() - lastYandexRequest < 55e3) {
+            if(lastYandexRequest > lastLocalYandexRequest) lastLocalYandexRequest = lastYandexRequest; // set local from d1
             throw error(429, "You are sending too many requests! Please respect the crawl-delay")
         } else {
             lastLocalYandexRequest = Date.now();
